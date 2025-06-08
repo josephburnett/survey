ee3f5c19a528ed240f752364ee8ef479db51d44c
Joseph Burnett <josephburnett79@gmail.com>
2025-06-08 16:47:57 -0700

    Adding missing views

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,5 @@
 class Section < ApplicationRecord
   validates :name, presence: true
-  validates :prompt, presence: true
   
   belongs_to :user, optional: true
   has_and_belongs_to_many :questions
