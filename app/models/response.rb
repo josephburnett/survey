1d6f0de0e3d5e41cf10ae0981be3dbc46b294cdf
Joseph Burnett <josephburnett79@gmail.com>
2025-06-16 16:02:42 -0700

    Also soft delete answers to a response

--- app/models/response.rb
+++ app/models/response.rb
@@ -6,6 +6,9 @@ class Response < ApplicationRecord
   scope :not_deleted, -> { where(deleted: false) }
   
   def soft_delete!
-    update!(deleted: true)
+    transaction do
+      answers.not_deleted.each(&:soft_delete!)
+      update!(deleted: true)
+    end
   end
 end
