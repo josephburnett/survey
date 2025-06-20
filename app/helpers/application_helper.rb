19566264a43caf48708c7264880b19917ecc4085
Joseph Burnett <josephburnett79@gmail.com>
2025-06-20 12:47:50 -0700

    Filter out namespaces without entities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,18 @@ module ApplicationHelper
   def all_namespaces_for_user(user)
     namespaces = Set.new
     
-    # Collect namespaces from all namespaceable models
+    # Collect all possible namespaces from all namespaceable models
     [Form, Section, Question, Answer, Response, Metric, Dashboard].each do |model|
       namespaces.merge(model.namespaces_for_user(user))
     end
     
-    namespaces.to_a.sort
+    # Filter out namespaces that don't actually have any entities
+    active_namespaces = namespaces.select do |namespace|
+      [Form, Section, Question, Answer, Response, Metric, Dashboard].any? do |model|
+        model.where(user: user, namespace: namespace).exists?
+      end
+    end
+    
+    active_namespaces.sort
   end
 end
