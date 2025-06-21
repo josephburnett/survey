f08eda35c860b0dd646c1082b22768c06951eca7
Joseph Burnett <josephburnett79@gmail.com>
2025-06-20 17:27:34 -0700

    Stay in namespace during navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,43 @@ module ApplicationHelper
     
     active_namespaces.sort
   end
+  
+  def current_namespace
+    # Try to get namespace from various sources in order of preference
+    if instance_variable_defined?('@current_namespace')
+      @current_namespace
+    elsif instance_variable_defined?('@form') && @form
+      @form.namespace
+    elsif instance_variable_defined?('@section') && @section
+      @section.namespace
+    elsif instance_variable_defined?('@question') && @question
+      @question.namespace
+    elsif instance_variable_defined?('@answer') && @answer
+      @answer.namespace
+    elsif instance_variable_defined?('@response') && @response
+      @response.namespace
+    elsif instance_variable_defined?('@metric') && @metric
+      @metric.namespace
+    elsif instance_variable_defined?('@dashboard') && @dashboard
+      @dashboard.namespace
+    elsif instance_variable_defined?('@namespace') && @namespace
+      @namespace.name
+    elsif params[:namespace]
+      params[:namespace]
+    else
+      ''
+    end
+  end
+  
+  def namespace_aware_path(model_class, path_method)
+    namespace = current_namespace || ''
+    
+    # Check if the target model has items in the current namespace
+    if namespace.present? && current_user && model_class.where(user: current_user, namespace: namespace).exists?
+      send(path_method, namespace: namespace)
+    else
+      # Fall back to root namespace
+      send(path_method)
+    end
+  end
 end
